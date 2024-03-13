0e86dfaf8d03fa9c6b4510dea0b9ef540b1e91a9
Abel Lavieri <abel.lavieri@gmail.com>
2024-03-12 23:36:52 -0400

    Initial app render to test the first endpoint

---
+++ src/features/Budget/Budgets.tsx
@@ -0,0 +1,33 @@
+import Budget from "./Budget";
+import BudgetService from "../../services/BudgetService";
+import { useQuery } from "@tanstack/react-query";
+import type { BudgetType } from "../../utils/appTypes";
+
+const Budgets = () => {
+
+  const { 
+    error, 
+    status, 
+    data: budgetsData,
+    isLoading,
+  } = useQuery({
+    queryKey: ['budgetsData'],
+    queryFn: () => BudgetService.getBudgets(),
+  });
+
+  if (isLoading) return <div>Loading...</div>
+  if (status === 'error') return <div>{error.message}</div>
+
+  return (
+    <div>
+      <h1>Budgets</h1>
+      <ul>
+        {budgetsData.budgets.map((budget: BudgetType) => (
+          <li key={budget.id}><Budget {...budget} /></li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Budgets;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
 
